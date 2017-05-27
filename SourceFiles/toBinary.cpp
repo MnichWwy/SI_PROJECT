ddf840952bff6ca11046bb4c060ef8e9a838b96e
Hubert Kowalski <mnichwwy@gmail.com>
2017-05-27 20:40:44 +0200

    toBinary - fixed

--- SourceFiles/toBinary.cpp
+++ SourceFiles/toBinary.cpp
@@ -4,7 +4,14 @@ void toBinary()
 {
 	for (auto it = personVec[0].begin(); it != personVec[0].end(); it++)
 	{
-			*it = std::bitset<8>(*it).to_string();	
+		string temp=nullptr;
+		for (std::size_t i = 0; i < it->size(); ++i)
+		{
+			bitset<8> b(it->c_str()[i]);
+			temp += b.to_string();
+		}
+		*it = temp;
+
 	}
 
 	cout << "PERSON:" << endl;
