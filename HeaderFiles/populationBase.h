e74877617bebc72ed1369ac5706b40f1c0672f28
Voytos <wojto321@gmail.com>
2017-05-31 17:11:08 +0200

    tworzenie/wyświetlanie populacji

---
+++ HeaderFiles/populationBase.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bitset>
+#include <ctime>
+
+using namespace std;
+
+extern int populationSize;
+extern vector<string> population;
+
+void initPopulation();
+void showPopulation();
