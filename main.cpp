cddd98751e4bf48129bf06c7c9f1cd232fc82529
Voytos <wojto321@gmail.com>
2017-06-01 01:20:52 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "toBinary.h"
 #include "toGray.h"
 #include "populationBase.h"
+#include "crossovers.h"
 #include "menu.h"
 
 using namespace std;
@@ -15,35 +16,14 @@ vector<string> personVec[1];
 vector <pair<string, string>> pairVec[2];
 
 int populationSize = 10;
-//int bits_ = 6;
+int bestActualFitness;
+float crossingProb = 0.7;
 vector<int> fitness(populationSize,0);
 vector<string> actualPopulation(populationSize,"      ");
 vector<string> crossovers(populationSize, "      ");
 vector<string> mutations(populationSize, "      ");
 vector<string> newPopulation(populationSize, "      ");
-
-void fitnessFunction(int i)
-{
-	auto it1 = actualPopulation.begin();
-	auto it2 = fitness.begin();
-
-	switch (i)
-	case 1:
-	{
-		for (it1; it1 != actualPopulation.end(); it1++)
-		{
-			it2++;
-			//	cout << *it << endl;
-			string sTemp = *it1;
-			//int iTemp = atoi(sTemp.c_str());
-
-			//tu ogolnie bedzie przypisanie do fitness obliczonej wartosci z actualPopulation
-			//w innych case'ach inne funkcje oceny
-		}
-		break;
-	}
-	
-}
+vector<int> selectedIndex;
 
 
 int main(int argc, char** argv)
@@ -52,7 +32,12 @@ int main(int argc, char** argv)
 	//readFile();
 	initPopulation();
 	//mainMenu();
-	showPopulation(); //test
+	//showPopulation(); //test
+	fitnessFunction(1);
+	sortByFitness();
+	selectChromosome();
+	singlePointCrossover();
+	showInRow();
 	system("pause");
 	return 0;
 }
