387dd404ba29d579b263185d3f9d499c3366c0ed
Voytos <wojto321@gmail.com>
2017-05-31 17:12:20 +0200

    dodanie includów

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include <vector>
 #include <string>
 #include <stdlib.h>
+#include <ctime>
 #include "readFile.h"
 #include "toBinary.h"
 #include "toGray.h"
+#include "populationBase.h"
 #include "menu.h"
 
 using namespace std;
@@ -12,12 +14,16 @@ using namespace std;
 vector<string> personVec[1];
 vector <pair<string, string>> pairVec[2];
 
-
+int populationSize = 10;
+vector<string> population;
 
 int main(int argc, char** argv)
 {
 	system("cls");
-	readFile();
-	mainMenu();
+	//readFile();
+	initPopulation();
+	//mainMenu();
+	showPopulation(); //test
+	system("pause");
 	return 0;
 }
