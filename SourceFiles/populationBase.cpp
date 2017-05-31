be8156e498d853ad0e93934f80af5a147176923d
Voytos <wojto321@gmail.com>
2017-05-31 17:09:54 +0200

    Add files via upload
    tworzenie/wyświetlenie populacji

---
+++ SourceFiles/populationBase.cpp
@@ -0,0 +1,18 @@
+#include "populationBase.h"
+
+void initPopulation()
+{
+	srand(time(NULL));
+	for (int i = 0; i<populationSize; i++)
+	{
+		population.push_back(bitset<6>(rand() % 33).to_string());
+	}
+}
+
+void showPopulation()
+{
+	for (auto it = population.begin(); it != population.end(); it++)
+	{
+		cout << *it << endl;
+	}
+}
