1370e57d262479a39c5c163b717ab3b40f1da22d
Voytos <wojto321@gmail.com>
2017-05-31 20:45:13 +0200

    dostosowanie do nowych vectorow

--- HeaderFiles/populationBase.h
+++ HeaderFiles/populationBase.h
@@ -8,7 +8,13 @@
 using namespace std;
 
 extern int populationSize;
-extern vector<string> population;
+//extern int bits_;
+//extern vector<vector<string>> population;
+extern vector<int> fitness;
+extern vector<string> actualPopulation;
+extern vector<string> crossovers;
+extern vector<string> mutations;
+extern vector<string> newPopulation;
 
 void initPopulation();
 void showPopulation();
