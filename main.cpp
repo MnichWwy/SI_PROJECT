658ebc05b6424d127a8f6f37d216a2e6fc0c971c
Voytos <wojto321@gmail.com>
2017-05-31 20:43:18 +0200

    nowe vectory, funkcja oceny

--- main.cpp
+++ main.cpp
@@ -15,7 +15,36 @@ vector<string> personVec[1];
 vector <pair<string, string>> pairVec[2];
 
 int populationSize = 10;
-vector<string> population;
+//int bits_ = 6;
+vector<int> fitness(populationSize,0);
+vector<string> actualPopulation(populationSize,"      ");
+vector<string> crossovers(populationSize, "      ");
+vector<string> mutations(populationSize, "      ");
+vector<string> newPopulation(populationSize, "      ");
+
+void fitnessFunction(int i)
+{
+	auto it1 = actualPopulation.begin();
+	auto it2 = fitness.begin();
+
+	switch (i)
+	case 1:
+	{
+		for (it1; it1 != actualPopulation.end(); it1++)
+		{
+			it2++;
+			//	cout << *it << endl;
+			string sTemp = *it1;
+			//int iTemp = atoi(sTemp.c_str());
+
+			//tu ogolnie bedzie przypisanie do fitness obliczonej wartosci z actualPopulation
+			//w innych case'ach inne funkcje oceny
+		}
+		break;
+	}
+	
+}
+
 
 int main(int argc, char** argv)
 {
