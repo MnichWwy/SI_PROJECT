ddc45bc4c93f33b0f2743cc30de531b63a497d68
gd95 <grzesiek23101995@wp.pl>
2017-05-31 22:55:38 +0200

    Create mutation.h

---
+++ HeaderFiles/mutation.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bitset>
+#include <ctime>
+#include "populationBase.h"
+
+using namespace std;
+
+
+void scrambleMutation();
+void inversionMutation();
+void swapMutation();
+void bitflipMutation();
+
+		
