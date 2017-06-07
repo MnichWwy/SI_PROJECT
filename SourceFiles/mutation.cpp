2bab2d5e4cb368dc7386f8ded0ba5cc0cef0993c
gd95 <grzesiek23101995@wp.pl>
2017-06-07 18:50:43 +0200

    Update

--- SourceFiles/mutation.cpp
+++ SourceFiles/mutation.cpp
@@ -1,53 +1,218 @@
 #include "mutation.h"
 #include "populationBase.h"
 
-void inversionMutation()
+void inversionMutation() //bierze losowy ciąg i zamienia go by był tyłem do przodu, np: 10011 -> 11001
 {
 	mutations = actualPopulation;
-	int random_gene = (rand() % populationSize) + 0;
-	int random_bit1 = (rand() % 4) + 0;
-	int range = 6 - random_bit1;
-	int random_bit2 = (rand() % range) + random_bit1;
-	char tmp1, tmp2;
-	while (random_bit2 > random_bit1 || random_bit2 != random_bit1)
+	for (nextgene=0; nextgene<populationSize;nextgene++)
 	{
-		tmp1 = mutations[random_gene][random_bit1];
-		tmp2 = mutations[random_gene][random_bit2];
-		mutations[random_gene][random_bit2] = tmp1;
-		mutations[random_gene][random_bit1] = tmp2;
-		random_bit1++;
-		random_bit2--;
+		bitset<32> flipendo(mutations[nextgene]);
+		if (flipendo.to_string() != "11111111111111111111111111111111" || flipendo.to_string() !="11111111111111111111111111111111")  //powinno być po 32
+		{
+			if (rand() % 100 < mutationchance) //szansa na zmutowanie
+			{
+				random_bit1 = (rand() % 31) + 0;
+				range = 32 - random_bit1;
+				random_bit2 = (rand() % range) + random_bit1; //losuje z bitseta miejsce startu i końca ciągu
+				char tmp1;
+				x=random_bit1;
+				y=random_bit2;
+				while (random_bit2 > random_bit1 || random_bit2 != random_bit1)
+				{
+					tmp1 = mutations[nextgene][random_bit1];
+					mutations[nextgene][random_bit1] = mutations[nextgene][random_bit2];
+					mutations[nextgene][random_bit2] = tmp1;
+					random_bit1++;
+					random_bit2--;
+				}
+				
+					
+				int i;
+				hOut = GetStdHandle( STD_OUTPUT_HANDLE );
+				SetConsoleTextAttribute( hOut, 0 );
+				for (i=0;i<x;i++)
+				{
+					cout<<actualPopulation[nextgene][i];
+				}
+				SetConsoleTextAttribute( hOut, 6 );
+				cout<<actualPopulation[nextgene][x];
+				SetConsoleTextAttribute( hOut, 0 );
+				for (i=x+1;i<y;i++)
+				{
+					cout<<actualPopulation[nextgene][i];
+				}
+				SetConsoleTextAttribute( hOut, 6 );
+				cout<<actualPopulation[nextgene][y];
+				SetConsoleTextAttribute( hOut, 0 );
+				for (i=y+1;i<32;i++)
+				{
+					cout<<actualPopulation[nextgene][i];
+				}
+				cout<<" przemutował w gen";
+				for (i=0;i<x;i++)
+				{
+					cout<<mutations[nextgene][i];
+				}
+				SetConsoleTextAttribute( hOut, 6 );
+				cout<<actualPopulation[nextgene][x];
+				SetConsoleTextAttribute( hOut, 0 );
+				for (i=x+1;i<y;i++)
+				{
+					cout<<mutations[nextgene][i];
+				}
+				SetConsoleTextAttribute( hOut, 6 );
+				cout<<mutations[nextgene][y];
+				SetConsoleTextAttribute( hOut, 0 );
+				for (i=y+1;i<32;i++)
+				{
+					cout<<mutations[nextgene][i];
+				}
+			}
+		}
 	}
-	cout << "Mutacja nastąpiła w genie " << actualPopulation[random_gene] << "zamieniając go na gen " << mutations[random_gene] << endl;
+
 	system("pause");
 
 }
 
-void swapMutation()
+void swapMutation() //zamienia miejscami dwa losowe bity
 {
 	mutations = actualPopulation;
-	int random_gene = (rand() % populationSize) + 0;
-	int random_bit1 = (rand() % 5) + 0;
-	int random_bit2 = (rand() % 5) + 0;
-	while (random_bit1 == random_bit2)
+	for (nextgene=0; nextgene<populationSize;nextgene++)
 	{
-		random_bit2 = (rand() % 5) + 0;
+		bitset<32> flipendo(mutations[nextgene]);
+		if (flipendo.to_string() != "11111111111111111111111111111111" || flipendo.to_string() !="11111111111111111111111111111111")  //powinno być po 32
+		{
+			if (rand() % 100 < mutationchance)
+			{
+				random_bit1 = (rand() % 5) + 0;
+		
+				do
+				{
+					random_bit2 = (rand() % 32) + 0;
+				}while (random_bit1 == random_bit2)
+				
+				//sprawdza czy nie wylosował takich samych, jak tak to losuje dalej
+	
+				while (mutations[nextgene][random_bit1] == mutations[nextgene][random_bit2])
+				{
+					do
+					{
+					random_bit2 = (rand() % 32) + 0;
+					}while (random_bit1 == random_bit2)
+				}
+				
+				//sprawdza czy wartości nie są takie same, jak tak to losuje dalej
+	
+				char tmp = mutations[nextgene][random_bit1];
+				mutations[nextgene][random_bit1] = mutations[nextgene][random_bit2];
+				mutations[nextgene][random_bit2] = tmp;
+				
+				int x,y;
+				if (random_bit1>random_bit2)
+				{
+					x=random_bit2;
+					y=random_bit1;
+				}
+				else
+				{
+					x=random_bit1;
+					y=random_bit2;
+				}
+					
+				int i;
+				hOut = GetStdHandle( STD_OUTPUT_HANDLE );
+				SetConsoleTextAttribute( hOut, 0 );
+				for (i=0;i<x;i++)
+				{
+					cout<<actualPopulation[nextgene][i];
+				}
+				SetConsoleTextAttribute( hOut, 6 );
+				cout<<actualPopulation[nextgene][x];
+				SetConsoleTextAttribute( hOut, 0 );
+				for (i=x+1;i<y;i++)
+				{
+					cout<<actualPopulation[nextgene][i];
+				}
+				SetConsoleTextAttribute( hOut, 6 );
+				cout<<actualPopulation[nextgene][y];
+				SetConsoleTextAttribute( hOut, 0 );
+				for (i=y+1;i<32;i++)
+				{
+					cout<<actualPopulation[nextgene][i];
+				}
+				cout<<" przemutował w gen";
+				for (i=0;i<x;i++)
+				{
+					cout<<mutations[nextgene][i];
+				}
+				SetConsoleTextAttribute( hOut, 6 );
+				cout<<actualPopulation[nextgene][x];
+				SetConsoleTextAttribute( hOut, 0 );
+				for (i=x+1;i<y;i++)
+				{
+					cout<<mutations[nextgene][i];
+				}
+				SetConsoleTextAttribute( hOut, 6 );
+				cout<<mutations[nextgene][y];
+				SetConsoleTextAttribute( hOut, 0 );
+				for (i=y+1;i<32;i++)
+				{
+					cout<<mutations[nextgene][i];
+				}
+	
+			}
+		}
 	}
-	char tmp = mutations[random_gene][random_bit1];
-	mutations[random_gene][random_bit1] = mutations[random_gene][random_bit2];
-	mutations[random_gene][random_bit2] = tmp;
-	cout << "Mutacja nastąpiła w genie " << actualPopulation[random_gene] << " zamieniając go na gen " << mutations[random_gene] << " za pomocą zamiany bitu" << random_bit1 <<"na"<< random_bit2 << endl;
+	
+
 	system("pause");
 }
 
-void bitflipMutation()
+void bitflipMutation() // zamienia bit z 0 na 1 lub z 1 na 0
 {
 	mutations = actualPopulation;
-	int random_bit = (rand() % 5) + 0;
-	int random_gene = (rand() % populationSize) + 0;
-	bitset<6> flipendo(mutations[random_gene]);
-	flipendo.flip(random_bit);
-	mutations[random_gene] = flipendo.to_string();
-	cout << "Mutacja nastąpiła w genie" << actualPopulation[random_gene] << "zamieniając go na gen" << mutations[random_gene] << "za pomocą negacji bitu" << random_bit << endl;
+	for (nextgene=0; nextgene<populationSize;nextgene++)
+	{
+	if (rand() % 100 < mutationchance)
+	{
+	random_bit1 = (rand() % 32) + 0;
+	bitset<32> flipendo(mutations[nextgene]);
+	flipendo.flip(random_bit1);
+	mutations[nextgene] = flipendo.to_string();
+	}
+	
+	int i;
+	hOut = GetStdHandle( STD_OUTPUT_HANDLE );
+	SetConsoleTextAttribute( hOut, 0 );
+	for (i=0;i<random_bit1;i++)
+	{
+		cout<<actualPopulation[nextgene][i];
+	}
+	SetConsoleTextAttribute( hOut, 6 );
+	cout<<actualPopulation[nextgene][random_bit1];
+	SetConsoleTextAttribute( hOut, 0 );
+	for (i=random_bit1+1;i<32;i++)
+	{
+		cout<<actualPopulation[nextgene][i];
+	}
+	cout<<" przemutował w gen";
+	for (i=0;i<random_bit1;i++)
+	{
+		cout<<mutations[nextgene][i];
+	}
+	SetConsoleTextAttribute( hOut, 6 );
+	cout<<mutations[nextgene][random_bit1];
+	SetConsoleTextAttribute( hOut, 0 );
+	for (i=random_bit1+1;i<32;i++)
+	{
+		cout<<mutations[nextgene][i];
+	}
+	}
 	system("pause");
+}
+
+void changeMutationChance(int much)
+{
+	mutationchance = much;
 }
