53ab0bf595d58bb4de8633de8beb3acd3b2996e5
gd95 <grzesiek23101995@wp.pl>
2017-06-07 18:48:53 +0200

    Update

--- HeaderFiles/mutation.h
+++ HeaderFiles/mutation.h
@@ -4,14 +4,22 @@
 #include <string>
 #include <bitset>
 #include <ctime>
+#include <stdlib.h>
+#include <windows.h>
+#include <iostream>
 #include "populationBase.h"
 
 using namespace std;
 
+extern int mutationchance;
+extern int random_bit1;
+extern int random_bit2;
+extern int nextgene;
+extern int range;
+
+HANDLE hOut;
 
 void scrambleMutation();
 void inversionMutation();
 void swapMutation();
-void bitflipMutation();
-
-		
+void bitflipMutation();	
